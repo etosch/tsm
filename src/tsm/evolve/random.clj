48540e4df6d3a3eaf1526ae18315c9278d35309a
Emma Tosch <etosch@gmail.com>
2012-08-16 10:59:12 -0400

    cleaned up random.clj

--- src/tsm/evolve/random.clj
+++ src/tsm/evolve/random.clj
@@ -4,22 +4,37 @@
   (:use [tsm.core]
 	[tsm.config]))
 
-(def instructions (assoc @push/instruction-table
-		    :noop :noop
-		    'rand-int pushrand/lrand-int
-		    'rand-float pushrand/lrand))
+(def instructions (seq (assoc @push/instruction-table
+			 :noop :noop
+			 'rand-int pushrand/lrand-int
+			 'rand-float pushrand/lrand)))
 
-;; rather ugly, since i wrote this at the pub; should be cleaned up when sober
-(defn random-code [code-size inst]
+(defn is-rand-inst? [thing] (cond (= thing 'rand-int) @max-int
+				  (= thing 'rand-float) @max-float
+				  :else false))
+(defn is-tag-inst? [thing] (or (= 'ts_tag thing) (= 'ts_tagged thing)))
+
+(defn random-code [code-size]
   (let [sans-pair (loop [x '() remaining-points code-size]
-		    (if (= 0 remaining-points)
-		      x
-		      (let [[n v] (pushrand/lrand-nth (seq inst))]
-			(cond (or (= n 'rand-int) (= n 'rand-float)) (recur (conj x (v 10)) (dec remaining-points))
-			      (or (= 'ts_tag n) (= 'ts_tagged n)) (recur (conj x {:imap n :tag (pushrand/lrand 10) :ith (inc (pushrand/lrand-int 2))}) (dec remaining-points))
-			      (meta v) (recur (conj x (reduce #(assoc %1 (%2 0) ((%2 1))) {:imap n} (seq (meta v)))) (dec remaining-points))
-			      :else (recur (conj x n) (dec remaining-points))))))]
+		    (if (= 0 remaining-points) x
+			(let [[n v] (pushrand/lrand-nth instructions)
+			      points (dec remaining-points)]
+			  (if-let [max-num (is-rand-inst? n)]
+			    (recur (conj x (v max-num)) points)
+			    (cond (is-tag-inst? n) (recur (conj x {:imap n
+								   :tag (pushrand/lrand @tag-limit)
+								   :ith (inc (pushrand/lrand-int 2))})
+							  points)
+				  (meta v) (recur (conj x (reduce #(assoc %1 (%2 0) ((%2 1)))
+								  {:imap n}
+								  (seq (meta v))))
+						  points)
+				  :else (recur (conj x n) points))))))]
+    ;; now pair according to uniform probability of all instructions
     (loop [before '() after sans-pair]
       (cond (empty? after) (vec before)
-	    (and (= 0 (pushrand/lrand-int @max-int)) (> (count after) 1)) (recur (conj before (vec (take 2 after))) (drop 2 after))
+	    (and (= 0 (pushrand/lrand-int (inc (count instructions))))
+		 (> (count after) 1))
+	           (recur (conj before (vec (take 2 after)))
+			  (drop 2 after))
 	    :else (recur (conj before (first after)) (rest after))))))
