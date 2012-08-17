d31139e4042831d524400ecd05560eebc558154f
Emma Tosch <etosch@gmail.com>
2012-08-17 18:21:35 -0400

    added main to project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject tsm "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :main tsm.evolve.gp)
