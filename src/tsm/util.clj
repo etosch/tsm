736da7f81b43f410b2279097902ad123a0696fe4
Emma Tosch <etosch@gmail.com>
2012-08-28 21:09:38 -0400

    added some logical operators and a new util funciton

--- src/tsm/util.clj
+++ src/tsm/util.clj
@@ -7,3 +7,21 @@
   (cond (> (if (neg? pivot) (- pivot) pivot) (count v)) [[] v]
 	(pos? pivot) [(subvec v 0 pivot) (subvec v pivot)]
 	:else [(subvec v 0 (+ (count v) pivot)) (subvec v (+ (count v) pivot))]))
+
+(defn exists
+  [coll & {pred :pred}]
+  (when (not (empty? coll))
+    (or ((or pred identity) (first coll))
+	(recur (rest coll) {:pred pred}))))
+
+(defn forall
+  [coll & {pred :pred}]
+  (or (empty? coll)
+      (and ((or pred identity) (first coll))
+	   (recur (rest coll) {:pred pred}))))
+
+(defn veclist-to-map
+  "Takes as its argument a list of vectors of size 2 and returns a sorted map where the first elements of the vectors are keys and the second elements are values.
+e.g. (veclist-to-map '([:a 0][:b 1][:c 2])) returns {:a 0, :b 1, :c 2}."
+  [vec-list]
+  (reduce #(assoc %1 (%2 0) (%2 1)) (sorted-map) vec-list))
