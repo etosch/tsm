42560886d21f0156de77635f4b0bd992faa99f44
Emma Tosch <etosch@gmail.com>
2012-08-30 21:48:25 -0400

    forgot to update boolean instructions to use the native instruction management

--- src/tsm/instructions/boolean.clj
+++ src/tsm/instructions/boolean.clj
@@ -1,22 +1,21 @@
 (ns tsm.instructions.boolean
-  (:use [tsm core util])
-  (:require [clojush.pushstate :as push]))
+  (:use [tsm core util instructions]))
 
-(push/define-registered boolean_and
+(define-registered boolean_and
   (fn [{boolstack :boolean, :as state}]
     (if (> (count boolstack) 1)
       (let [[boolstack [phi psi]] (vec-split boolstack -2)]
 	(add-to-stack (assoc state :boolean boolstack) :boolean (and phi psi)))
       state)))
 
-(push/define-registered boolean_or
+(define-registered boolean_or
   (fn [{boolstack :boolean, :as state}]
     (if (> (count boolstack) 1)
       (let [[boolstack [phi psi]] (vec-split boolstack -2)]
 	(add-to-stack (assoc state :boolean boolstack) :boolstack (or phi psi)))
       state)))
 
-(push/define-registered boolean_not
+(define-registered boolean_not
   (fn [{boolstack :boolean, :as state}]
     (if (> (count boolstack) 0)
       (let [[boolstack [phi]] (vec-split boolstack -1)]
